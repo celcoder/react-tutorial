57081821f60f5389ce2a51a1b82abb6c993d3394
Chang Ethan Lee <celcoder@gmail.com>
2016-09-22 23:58:42 -0400

    added constructor with click event handler

--- components/src/js/components/Card.js
+++ components/src/js/components/Card.js
@@ -2,14 +2,33 @@ import React from 'react';
 import CheckList from './CheckList';
 
 export default class Card extends React.Component {
+  constructor() {
+    super()
+    this.state = {
+      showDetails: false
+    }
+  }
+
+  toggle() {
+    const showDetails = !this.state.showDetails;
+    this.setState({showDetails});
+  }
+
   render() {
-    return (
-      <div className='card'>
-        <div className='card__title'>{this.props.title}</div>
-        <div className='card__details'>
+    let cardDetails;
+    if(this.state.showDetails) {
+      cardDetails = (
+        <div className="card__details">
           {this.props.description}
           <CheckList cardId={this.props.id} tasks={this.props.tasks} />
         </div>
+      )
+    }
+    
+    return (
+      <div className='card'>
+        <div className='card__title' onClick={this.toggle.bind(this)}>{this.props.title}</div>
+        {cardDetails}
       </div>
     )
   }
